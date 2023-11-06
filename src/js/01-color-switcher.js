29041cbd9e7709025caab884ec02e0a4b31dda3a
JenyaOst <gjhit922@gmail.com>
2023-11-06 17:48:02 +0200

    Change4

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,25 +2,27 @@
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 const colorBody = document.querySelector('body');
-btnStop.setAttribute('disabled', true) ;
-let intervalId ;
-btnStart.addEventListener('click', onClick);
-btnStop.addEventListener('click', offClick);
+let timerId;
+
+btnStop.setAttribute("disabled", true);
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
-function onClick (element){
-  const initialColor = getRandomHexColor();
-colorBody.style.backgroundColor = initialColor;
-  intervalId =  setInterval(() => {
-const initialColor = getRandomHexColor();
-colorBody.style.backgroundColor = initialColor;
-}, 1000  );
-btnStop.removeAttribute('disabled');
-element.target.setAttribute('disabled', true) ;
-};
-function offClick (element){
-    clearInterval(intervalId);
-    btnStart.removeAttribute('disabled');
-    element.target.setAttribute('disabled', true) ;
-};
+
+btnStart.addEventListener('click', (element) => {
+    const initialColor = getRandomHexColor();
+    colorBody.style.backgroundColor = initialColor;
+
+    timerId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        colorBody.style.backgroundColor = randomColor;
+  }, 1000);
+  element.target.setAttribute("disabled", true);
+  btnStop.removeAttribute("disabled");
+});
+
+btnStop.addEventListener('click', (element) => {
+    clearInterval(timerId);
+    element.target.setAttribute("disabled", true);
+    btnStart.removeAttribute("disabled");
+});
